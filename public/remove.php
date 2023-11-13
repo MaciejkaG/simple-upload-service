28dfa8a938f445ee662fcd54e84fa635ddde084d
Maciej Gomoła <maciejg2008@gmail.com>
2023-11-13 12:39:44 +0100

    improved comments

--- public/remove.php
+++ public/remove.php
@@ -2,9 +2,9 @@
     $filesDir = "../files/"; // Musi kończyć się slashem!
     $redirectRoute = "/upload"; // Adres względny do któego ma przekierować, jeżeli masz index.php w public/upload to powinno być to /upload
 
-    $targetFile = $filesDir . basename($_GET["fname"]); // Ustawienie zmiennej $targetFile odpowiedniej $filesDir + nazwa przesłanego pliku
+    $targetFile = $filesDir . basename($_GET["fname"]); // Ustawienie zmiennej $targetFile czyli ścieżka pliku do usunięcia
     if (file_exists($targetFile)) {
-        if (unlink($targetFile)) { // Skopiuj zuploadowany plik do poprawnego miejsca na serwerze
+        if (unlink($targetFile)) { // Usuń plik z serwera
             http_response_code(200);
             header('Location: '.$redirectRoute); // Przekieruj z powrotem do strony głównej
         } else { // W razie gdyby coś nie wyszło po stronie serwera podczas kopiowania pliku
